3483d982610997b50fc4d69a5e5943974eb4cccc
Irina <115542977+IrinaGaripova@users.noreply.github.com>
2022-10-25 11:16:39 +0500

    Tasks

---
+++ tasks04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти расстояние между точками в пространстве 2D
+Console.Clear();
+Console.Write("Ddtlbnt x1 :");
+int x1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Ddtlbnt y1 :");
+int y1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Ddtlbnt x2 :");
+int x2 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Ddtlbnt y2 :");
+int y2 = int.Parse(Console.ReadLine() ?? "0");
+double distance = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+Console.WriteLine("Расстояние между точками равно: ");
+Console.WriteLine(distance);
