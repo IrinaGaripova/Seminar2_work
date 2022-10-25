d25a2e9a13c5af4acedd298215efa9210b17c111
Irina <115542977+IrinaGaripova@users.noreply.github.com>
2022-10-25 11:08:35 +0500

    Taska

---
+++ taska03/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Clear();
+Console.WriteLine("Определить номер четверти");
+
+Console.WriteLine("Введите первую координату: ");
+int x = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введитt вторую координату: ");
+int y = int.Parse(Console.ReadLine() ?? "0");
+if (x>0 && y>0)
+{
+  Console.WriteLine("Номер четверти - 1");
+}
+
+else if (x<0 && y>0)
+{
+    Console.WriteLine("Номер четверти - 2");   
+}
+else if (x<0 && y<0)
+{
+    Console.WriteLine("Номер четверти - 3"); 
+}
+else if (x>0 && y<0)
+{
+    Console.WriteLine("Номер четверти - 4"); 
+}
